91ebb97ab4bea8851446d3c386fc6cc9eba8ea07
Daud <85739147+RedDead11@users.noreply.github.com>
2026-02-25 07:36:54 +0500

    Checks DB if shortened link already exists and returns that instead of creating a new one

--- backend/src/services/service.ts
+++ backend/src/services/service.ts
@@ -1,8 +1,17 @@
-import { URLMapping } from "../models/model";
 import { generateShortCode } from "../utils/generateShortCode";
-import {prisma} from "../lib/prisma";
+import { prisma } from "../lib/prisma";
 
-export const createShortUrlService = async (originalUrl: string): Promise<string> => {
+export const createShortUrlService = async (
+  originalUrl: string,
+): Promise<string> => {
+  // Check if url is already shortened in db
+  const existing = await prisma.url.findFirst({
+    where: { originalUrl },
+  });
+
+  if (existing) return existing.shortCode;
+
+  // Otherwise create new one
   const shortCode = generateShortCode();
 
   await prisma.url.create({
@@ -19,16 +28,16 @@ export const createShortUrlService = async (originalUrl: string): Promise<string
 
 export const getUrlService = async (shortCode: string) => {
   return await prisma.url.findUnique({
-    where: {shortCode},
+    where: { shortCode },
   });
 };
 
 // Increment clicks
 export const incrementClicksService = async (shortCode: string) => {
   await prisma.url.update({
-    where: {shortCode},
+    where: { shortCode },
     data: {
-      clicks: {increment: 1},
+      clicks: { increment: 1 },
     },
   });
 };
