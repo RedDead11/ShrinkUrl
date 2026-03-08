145fba177d56f5c13bca208bea582ca14c00ef95
Daud <85739147+RedDead11@users.noreply.github.com>
2026-03-08 13:35:45 +0500

    Health Check

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
-import { createShortUrl, getStats, redirectShortUrl } from "../controllers/controller";
+import {
+  createShortUrl,
+  getStats,
+  redirectShortUrl,
+} from "../controllers/controller";
 
 const router = Router();
 
+// health check
+router.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 // Order matters
 router.post("/shorten", createShortUrl); // create short URL
-router.get("/stats/:shortCode", getStats);  // stats
+router.get("/stats/:shortCode", getStats); // stats
 router.get("/:shortCode", redirectShortUrl); // redirect
 
 export default router;
